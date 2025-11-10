46ac4a87b2d243b14692507feac0d67d7657e256
Luciano Joaquin Maciel <105524431+LucianoMaciel10@users.noreply.github.com>
2025-11-10 17:14:03 -0300

    First commit

---
+++ app/auth/login/page.tsx
@@ -0,0 +1,16 @@
+import { BookmarkIcon } from "@radix-ui/react-icons";
+import { Button } from "@radix-ui/themes";
+
+function LoginPage() {
+  return (
+    <div>
+      LoginPage
+      <Button>
+        <BookmarkIcon />
+        Login
+      </Button>
+    </div>
+  );
+}
+
+export default LoginPage;
